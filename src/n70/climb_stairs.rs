e3f4c43a9e53c3b21056254f5e0576f2bedf7e76
franc <francismajere@gmail.com>
2022-09-29 18:16:37 +0800

    n70

--- src/n70/climb_stairs.rs
+++ src/n70/climb_stairs.rs
@@ -1,3 +1,13 @@
 pub fn climb_stairs(n: i32) -> i32 {
-    0
+    if n == 1 {
+        return 1;
+    }
+    let mut dp = Vec::with_capacity(n as usize + 1);
+    dp.push(0);
+    dp.push(1);
+    dp.push(2);
+    for i in 3..n + 1 {
+        dp.push(dp[i as usize - 1] + dp[i as usize - 2]);
+    }
+    *dp.last().unwrap()
 }
